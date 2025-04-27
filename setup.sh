4dd0dfd004e2dc35d0e598e13db70923a00bd0d4
Sam Richards <sammyrichards@me.com>
2025-04-26 20:52:13 -0700

    refactor: clean up .bashrc formatting and update setup script permissions

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
+sudo apt update && sudo apt install -y git curl unzip
+curl -s https://ohmyposh.dev/install.sh | bash -s
+curl -LsSf https://astral.sh/uv/install.sh | sh
 cd ..
 rm .bashrc
 ln -s ~/dotfiles/.bashrc ~/.bashrc
+oh-my-posh font install
